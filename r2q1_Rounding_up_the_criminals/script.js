9a3a18d0e911f1e77273462e5f6163ae0036f416
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-05-24 09:55:56 +0000

    add new

--- r2q1_Rounding_up_the_criminals/script.js
+++ r2q1_Rounding_up_the_criminals/script.js
@@ -1,3 +1,35 @@
 // PLACE YOUR CODE HERE
 
+// PLACE YOUR CODE HERE
 
+
+
+let wantedList = document.querySelectorAll('div.a');
+
+for (let wanted of wantedList) {
+    let taskElement = document.createElement('span');
+    console.log(taskElement)
+    taskElement.innerHTML = "jailed"
+    console.log(taskElement.innerHTML)
+    wanted.appendChild(taskElement)
+    wanted.style.backgroundColor = "red"
+//    let list = document.querySelectorAll('div')
+
+    // get the current name 
+    // let currentName = wanted.innerHTML
+    // console.log(currentName)
+    // let currentName = wanted.appendChild("span")
+    
+    // modify with jail
+    // (currentName + " jailed").innerHTML
+    // replace the current with the modify
+    // wanted.innerHTML = currentName
+    // console.log(newText)
+}
+
+
+let coverList = document.querySelectorAll('div.box.a.undercover');
+
+for (let cover of coverList) {
+    cover.style.backgroundColor = "yellow"
+}
